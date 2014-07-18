c477ff229c3c96de749987298002f57d523710e5
Andrea Ascari <andrea@70division.com>
2014-07-18 10:03:30 +0200

    Create the circlebuttonview.

--- src/com/seventydivision/framework/views/CircleButton.java
+++ src/com/seventydivision/framework/views/CircleButton.java
@@ -1,23 +1,17 @@
 package com.seventydivision.framework.views;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.seventydivision.framework.utils.Utils;
 
 /**
- * Created by andreaascari on 27/01/14.
+ * Created by andreaascari on 18/07/14.
  */
-public class CircleButton extends Button {
-
+public class CircleButton extends ImageButton {
 
     public CircleButton(Context context) {
         super(context);
@@ -33,66 +27,6 @@ public class CircleButton extends Button {
 
     @Override
     protected void onDraw(Canvas canvas) {
-
-        Drawable drawable = getBackground();
-
-        if (drawable == null) {
-            return;
-        }
-
-        if (getWidth() == 0 || getHeight() == 0) {
-            return;
-        }
-
-        if (drawable instanceof BitmapDrawable) {
-
-            Bitmap b = ((BitmapDrawable) drawable).getBitmap();
-
-            Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
-
-            int w = getWidth(), h = getHeight();
-
-            Bitmap roundBitmap = getCroppedBitmap(bitmap, w);
-            canvas.drawBitmap(roundBitmap, 0, 0, null);
-        } else {
-            super.onDraw(canvas);
-        }
-
+        Utils.Views.drawCircle(this, canvas);
     }
-
-    public static Bitmap getCroppedBitmap(Bitmap bmp, int radius) {
-        Bitmap sBmp;
-
-        if (bmp.getWidth() != radius || bmp.getHeight() != radius) {
-            float smallest = Math.min(bmp.getWidth(), bmp.getHeight());
-            float factor = smallest / radius;
-            sBmp = Bitmap.createScaledBitmap(bmp, (int) (bmp.getWidth() / factor), (int) (bmp.getHeight() / factor), false);
-        } else {
-            sBmp = bmp;
-        }
-
-        Bitmap output = Bitmap.createBitmap(radius, radius,
-                Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
-
-        final int color = 0xffa19774;
-        final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, radius, radius);
-
-        paint.setAntiAlias(true);
-        paint.setFilterBitmap(true);
-        paint.setDither(true);
-        canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(Color.parseColor("#BAB399"));
-        canvas.drawCircle(
-                radius / 2 + 0.7f,
-                radius / 2 + 0.7f,
-                radius / 2 + 0.1f,
-                paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(sBmp, rect, rect, paint);
-
-        return output;
-    }
-
 }
--- src/com/seventydivision/framework/views/CircleImage.java
+++ src/com/seventydivision/framework/views/CircleImage.java
@@ -12,6 +12,9 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 
+import com.facebook.android.Util;
+import com.seventydivision.framework.utils.Utils;
+
 /**
  * Created by andreaascari on 27/01/14.
  */
@@ -32,66 +35,7 @@ public class CircleImage extends PowerImageView {
 
     @Override
     protected void onDraw(Canvas canvas) {
-
-        Drawable drawable = getDrawable();
-
-        if (drawable == null) {
-            return;
-        }
-
-        if (getWidth() == 0 || getHeight() == 0) {
-            return;
-        }
-
-        if (drawable instanceof BitmapDrawable) {
-
-            Bitmap b = ((BitmapDrawable) drawable).getBitmap();
-
-            Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
-
-            int w = getWidth(), h = getHeight();
-
-            Bitmap roundBitmap = getCroppedBitmap(bitmap, w);
-            canvas.drawBitmap(roundBitmap, 0, 0, null);
-        } else {
-            super.onDraw(canvas);
-        }
-
-    }
-
-    public static Bitmap getCroppedBitmap(Bitmap bmp, int radius) {
-        Bitmap sBmp;
-
-        if (bmp.getWidth() != radius || bmp.getHeight() != radius) {
-            float smallest = Math.min(bmp.getWidth(), bmp.getHeight());
-            float factor = smallest / radius;
-            sBmp = Bitmap.createScaledBitmap(bmp, (int) (bmp.getWidth() / factor), (int) (bmp.getHeight() / factor), false);
-        } else {
-            sBmp = bmp;
-        }
-
-        Bitmap output = Bitmap.createBitmap(radius, radius,
-                Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
-
-        final int color = 0xffa19774;
-        final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, radius, radius);
-
-        paint.setAntiAlias(true);
-        paint.setFilterBitmap(true);
-        paint.setDither(true);
-        canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(Color.parseColor("#BAB399"));
-        canvas.drawCircle(
-                radius / 2 + 0.7f,
-                radius / 2 + 0.7f,
-                radius / 2 + 0.1f,
-                paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(sBmp, rect, rect, paint);
-
-        return output;
+        Utils.Views.drawCircle(this, canvas);
     }
 
 }
