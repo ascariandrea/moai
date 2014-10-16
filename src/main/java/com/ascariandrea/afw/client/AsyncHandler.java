ee7ba2f1f95da78d2784123f21f748fd3584bc56
Andrea Ascari <andrea@70division.com>
2014-10-16 20:23:53 +0200

    Implement android device token retrieving logic.

--- src/main/java/com/ascariandrea/afw/client/AsyncHandler.java
+++ src/main/java/com/ascariandrea/afw/client/AsyncHandler.java
@@ -2,4 +2,19 @@ package com.ascariandrea.afw.client;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
-public abstract class AsyncHandler extends AsyncHttpResponseHandler {}
+import org.apache.http.Header;
+
+public abstract class AsyncHandler extends AsyncHttpResponseHandler {
+
+
+    @Override
+    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+    }
+
+
+    @Override
+    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+    }
+}
