90b6b631c5585c61a4b0981f2214411b0053dde5
Andrea Ascari <andrea@70division.com>
2014-10-20 14:26:37 +0200

    Add gcm intent service and broadcast receiver to handle push notification.

---
+++ src/main/java/com/ascariandrea/afw/services/GCMBroadcastReceiver.java
@@ -0,0 +1,23 @@
+package com.ascariandrea.afw.services;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by andreaascari on 24/02/14.
+ */
+public class GCMBroadcastReceiver extends WakefulBroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Explicitly specify that GcmIntentService will handle the intent.
+        ComponentName comp = new ComponentName(context.getPackageName(), GCMIntentService.class.getName());
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+}
