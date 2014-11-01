b7a1b75f596af94df961399fa140252e65888f6f
Andrea Ascari <andrea@70division.com>
2014-11-01 17:12:23 +0100

    Optimize the structure and init the sample module.

---
+++ sample/src/main/java/com/ascariandrea/afw/samples/login/fragments/SplashFragment.java
@@ -0,0 +1,14 @@
+package com.ascariandrea.afw.samples.login.fragments;
+
+import com.ascariandrea.afw.fragments.InjectedFragment;
+
+/**
+ * Created by andreaascari on 01/11/14.
+ */
+public class SplashFragment extends InjectedFragment {
+    
+    @Override
+    public void onViewInjected() {
+
+    }
+}
