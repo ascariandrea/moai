87ba15055f06fc0c644eb64773ed5006df635e57
Andrea Ascari <andrea@70division.com>
2015-02-03 09:24:54 +0100

    Add custom button class for button with custom font.

---
+++ framework/src/main/java/com/ascariandrea/moai/views/CustomButton.java
@@ -0,0 +1,52 @@
+package com.ascariandrea.moai.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.ascariandrea.moai.R;
+
+/**
+ * Created by andreaascari on 20/01/15.
+ */
+public class CustomButton extends Button {
+    private static final String TAG = CustomButton.class.getSimpleName();
+
+    public CustomButton(Context context) {
+        super(context);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setCustomFont(context, attrs);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setCustomFont(context, attrs);
+    }
+
+    private void setCustomFont(Context ctx, AttributeSet attrs) {
+        TypedArray a = ctx.obtainStyledAttributes(attrs, R.styleable.CustomFontTxtView);
+        String customFont = a.getString(R.styleable.CustomFontTxtView_customFont);
+        setCustomFont(ctx, customFont);
+        a.recycle();
+    }
+
+
+    public boolean setCustomFont(Context ctx, String asset) {
+        Typeface tf;
+        try {
+            tf = Typeface.createFromAsset(ctx.getAssets(), asset);
+            setTypeface(tf);
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, "Error to get typeface: " + e.getMessage());
+        }
+        return false;
+    }
+}
